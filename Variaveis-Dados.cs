175cbc8ff9f4e8c331dd23a653dd04aab056debc
Thiago Fabri <nysevnine@gmail.com>
2025-03-17 13:37:23 -0300

    Add files via upload

---
+++ Variaveis-Dados.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main() 
+    {
+        int idade = 18;
+        float altura = 1.76f;
+        double peso = 75.5;
+        bool vivo = true;
+        char letra = 'T';
+        string nome = "Thiago";
+        
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Altura: " + altura);
+        Console.WriteLine("Peso: " + peso);
+        Console.WriteLine("Esta Vivo: " + vivo);
+        Console.WriteLine("Qual Letra: " + letra);
+        Console.WriteLine("Qual Nome: " + nome);
+    }
+}
